39a4ae27c7075223460dbc98e68ec619cd839410
Anne Patricio <69012296+annepatricio@users.noreply.github.com>
2023-07-18 11:59:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import math
 coordinatesFile = "ADD_HERE_THE_FILE_WITH_THE_CENTROIDS.xlsx";
 #The coordinatesFile must have at least 3 columns with the following information:(code,longitude,latitude)
 maxNumberOfCoordinatesPerGroup = 2;
-/*
+"""
 The maxNumberOfCoordinatesPerGroup must be defined so that all groups have the same number of coordinates. 
 For example, if you have 180 coordinates, the maxNumberOfCoordinatesPerGroup 36 (resulting in 5 groups of 36) 
 or 45 (resulting in 4 groups of 36). 
 I have successfully applied a maxNumberOfCoordinatesPerGroup up to 53 and retrieved a file with distances and durations between more than 1000 coordinates. 
-*/
+"""
 outputFile = "ADD_HERE_THE_OUTPUT_FILE.xlsx";
 
 
@@ -49,6 +49,9 @@ inicio = 'http://router.project-osrm.org/table/v1/driving/';
 sources = '?sources=';
 destinations = '&destinations=';
 fim = '&annotations=duration,distance';
+codes = []
+distances = []
+durations = []
 
 for o in range(numberOfGroups):
     grupoOrigem = coordinates[o];
